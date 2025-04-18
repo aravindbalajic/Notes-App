8e4f49452aa26569beddea5cc71f07b45d7b53db
aravindbalajic <aravindmail2424@gmail.com>
2025-04-18 17:27:12 +0530

    Initial commit with API Fetch and Data Rendered

---
+++ Backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const notes = require('./data/notes')
+const dotenv = require('dotenv');
+const connectDB = require('./models/db');
+const cors = require('cors')
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+dotenv.config();
+connectDB();
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello From backend");
+    console.log("Helo bknd");
+})
+
+app.get('/notes',(req,res)=>{
+    res.send(notes);
+    console.log(notes);
+})
+
+app.get("/notes/:id",(req,res)=>{
+    const note=notes.find((n)=>n._id === req.params.id);
+    res.send(note);
+    console.log(note);
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log("Server Started running at port:"+PORT);
+});
